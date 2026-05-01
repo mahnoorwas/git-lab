65719429cc494748882bd84171ea8d1ebe538c20
mahnoorwas <syedamahnoorwas@gmail.com>
2026-05-02 04:58:22 +0500

    Add biography website

---
+++ articles.js
@@ -0,0 +1,61 @@
+const commentForm = document.getElementById('comment-form');
+const commentsContainer = document.getElementById('comments-container');
+
+commentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const commentText = document.getElementById('comment-text').value;
+    const comment = document.createElement('div');
+    comment.classList.add('comment');
+    comment.textContent = commentText;
+    commentsContainer.appendChild(comment);
+    document.getElementById('comment-text').value = '';
+});
+
+
+function readMore1() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+function readMore2() {
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+function readMore3() {
+  var dots = document.getElementById("dots3");
+  var moreText = document.getElementById("more3");
+  var btnText = document.getElementById("myBtn3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
